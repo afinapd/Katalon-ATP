f54d95437c66223d9ec02efe29f569c6ba682665
afinapd <afnpd03@gmail.com>
2020-08-31 16:00:24 +0700

    [add] date picker, test suite, and report

---
+++ Test Suites/AllTestSuite.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>AllTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>9f7c7096-4b19-4ab5-b422-783ad35946cc</testSuiteGuid>
+   <testCaseLink>
+      <guid>659c6f73-fc0e-43bd-82a1-105c5784e125</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Alert</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e9671ac8-e3af-40d5-b49b-db5d93dba305</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DatePicker</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
